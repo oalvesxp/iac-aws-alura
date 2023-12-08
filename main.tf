3368a6b8c7cbb9560fc8a402d8a0275f85369336
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-07 22:29:58 -0300

    adding OS tag and disabled user_data

--- main.tf
+++ main.tf
@@ -18,14 +18,15 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0230bd60aa48260c6"
   instance_type = "t2.micro"
   key_name = "iac-alura"
-  user_data = <<-EOF
-        #!/bin/bash
-        cd /home/ubuntu
-        echo "<h1>Hello world!</h1></br><p>Created by Terraform.</p>" > index.html
-        nohup busybox httpd -f -p 8080 &
-  EOF
+#   user_data = <<-EOF
+#         #!/bin/bash
+#         cd /home/ubuntu
+#         echo "<h1>Hello world!</h1></br><p>Created by Terraform.</p>" > index.html
+#         nohup busybox httpd -f -p 8080 &
+#   EOF
 
   tags = {
     Name = "vm-p-wp01"
+    OS = "ubuntu"
   }
 }
