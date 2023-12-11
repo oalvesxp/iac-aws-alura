e48dfb4bfe10d49dca184975b13fd7c279019820
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-11 00:23:22 -0300

    changing max_size and module name

--- env/staging/main.tf
+++ env/staging/main.tf
@@ -1,9 +1,9 @@
-module "production" {
+module "staging" {
   source = "../../infra"
 
   repo_name        = "staging"
   repo_description = "application-of-staging"
   env              = "env-staging"
   instance_type    = "t2.micro"
-  max_size         = 5
+  max_size         = 1
 }
