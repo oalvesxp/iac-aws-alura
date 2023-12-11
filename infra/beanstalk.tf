9d496c6f5a6ca2b0bbc98a48f160a126098a6cbf
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-11 00:09:58 -0300

    adding benstalk_environment resource

--- infra/beanstalk.tf
+++ infra/beanstalk.tf
@@ -8,3 +8,27 @@ resource "aws_elastic_beanstalk_application" "application" {
   #     delete_source_from_s3 = true
   #   }
 }
+
+resource "aws_elastic_beanstalk_environment" "application" {
+  name                = var.env
+  application         = aws_elastic_beanstalk_application.application.name
+  solution_stack_name = "64bit Amazon Linux 2023 v4.1.1 running Docker"
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = var.instance_type
+  }
+
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MaxSize"
+    value     = var.max_size
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.beanstalk.name
+  }
+}
