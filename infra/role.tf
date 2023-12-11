6458d8d63d2d7ce1c288fe03fb16bb3353d4de42
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-10 23:41:01 -0300

    adding policies for beanstalk

--- infra/role.tf
+++ infra/role.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "beanstalk_ec2" {
-  name = "beanstalk"
+resource "aws_iam_role" "beanstalk" {
+  name = "beanstalk-ec2"
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
@@ -13,6 +13,41 @@ resource "aws_iam_role" "beanstalk_ec2" {
         Principal = {
           Service = "ec2.amazonaws.com"
         }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "beanstalk" {
+  name = "beanstalk-ec2"
+  role = aws_iam_role.beanstalk.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "cloudwatch:PutMetricData",
+          "ds:CreateComputer",
+          "ds:DescribeDirectories",
+          "ec2:DescribeInstanceStatus",
+          "logs:*",
+          "ssm:*",
+          "ec2messages:*",
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetRepositoryPolicy",
+          "ecr:DescribeRepositories",
+          "ecr:ListImages",
+          "ecr:DescribeImages",
+          "ecr:BatchGetImage",
+          "s3:*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
       },
     ]
   })
