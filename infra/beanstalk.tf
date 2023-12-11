df6fa8e2fc1e1ab9b5ced4d15de0b396ead6691f
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-10 23:53:31 -0300

    adding beanstalk resource

---
+++ infra/beanstalk.tf
@@ -0,0 +1,10 @@
+resource "aws_elastic_beanstalk_application" "application" {
+  name        = var.repo_name
+  description = var.repo_description
+
+  #   appversion_lifecycle {
+  #     service_role          = aws_iam_role.beanstalk.arn
+  #     max_count             = 128
+  #     delete_source_from_s3 = true
+  #   }
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -3,3 +3,8 @@ variable "repo_name" {
   description = "Name of repository ECR (Elastic Container Registry)"
   type        = string
 }
+
+variable "repo_description" {
+  description = "Description of repository ECR (Elastic Container Registry)"
+  type        = string
+}
