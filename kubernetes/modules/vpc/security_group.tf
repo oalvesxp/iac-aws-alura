16464cae963b5ac29c16a34c48b6546815e8dc3f
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-10 23:10:06 -0300

    adding security groups config

---
+++ kubernetes/modules/vpc/security_group.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "this" {
+  name        = "ssh_cluster"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "in" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
+  security_group_id = aws_security_group.this.id
+}
+
+resource "aws_security_group_rule" "out" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
+  security_group_id = aws_security_group.this.id
+}
